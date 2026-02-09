81ad4a9ce92a63232b5269103ef08d7696ea1ff8
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-09 23:32:49 +0300

    09-Access-Modifiers: access modifiers topic completed

---
+++ 09-Access_Modifiers/Kullanici.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _09_Access_Modifiers //namespace daima public olarak kalırlar ve başka bir erişim belirteci almazlar.
+{
+    public class Kullanici//Ana sınıflar sadece public ve internal erişim belirteçleri alabilir
+    {
+        public string Adi;//Program class ında bu değişkene ulaşmak için string in önüne public ekledik, yoksa orada erişemiyoruz
+        internal string Soyadi;//Bir diğer rişim yöntemi de string in önüne internal yazmak, böylece aynı projede olduğu için bu değişken de ulaşabilir hale geliyor
+        private string Telefon;//Buradaki telefon değişkeni ise private yaptığımız için program class ında gelmez ama bu class da kullanabiliriz
+        protected string Email;//Bu email değişkenine de program cs den erişemeyiz protected yaptığımız için, bu sınıfta veya buradan miras alan bir sınıfta kullanabiliriz
+        string sifre;
+    }
+    internal class Adres
+    {
+        private class Siparis //Ana sınıf içerisinde private ve internal erişim belirteci alabilir 
+        {
+
+        }
+    }
+    
+}
--- 09-Access_Modifiers/Program.cs
+++ 09-Access_Modifiers/Program.cs
@@ -8,8 +8,29 @@ namespace _09_Access_Modifiers
 {
     internal class Program
     {
+        public class deneme
+        {
+            public string UrunAdi = "public öğeye herkes erişebilir";
+            private string urunAdi;
+        }
+        protected class test // Ait olduğu sınıftan ve o sınıftan türetilen sınıflardan erişebilir
+        {
+            private string UrunAdi;
+        }
+        internal class urun // internal erişim belirteci Etkin projeye ait sınıflardan erişilebilir, erişilemez
+        {
+            private string gizli = "Private yalnız bulunduğu sınıftan erişebilir, dıştaki sınıflardan erişilemez";
+        }
         static void Main(string[] args)
         {
+            Kullanici kullanici = new Kullanici();
+            kullanici.Adi = "Enes";
+            kullanici.Soyadi = "Yerebakan";
+
+            Console.WriteLine($"Kullanıcı adı: {kullanici.Adi}");
+            Console.WriteLine($"Kullanıcı soyadı: {kullanici.Soyadi}");
+
+            Console.Read();
         }
     }
 }
