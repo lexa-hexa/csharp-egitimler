7ef0628e3a0aa45a5c6624db7c08b546352effec
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-19 12:07:41 +0300

    Inheritance practice: bank account system with polymorphism and overridden withdrawal fee

---
+++ 14-BankaHesapSistemi/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_BankaHesapSistemi //Bu bir örnek senaryodur. Gerçek bir banka hesap sistemi değildir. Sadece kalıtım konusunu pekiştirmek amacıyla hazırlanmıştır.
+{
+    class BankaHesabi
+    {
+        public string HesapNo;
+        public decimal Bakiye;
+        public void ParaYatir(decimal tutar)
+        {
+            if (tutar <= 0)
+            {
+                Console.WriteLine("Lütfen geçerli bir tutar giriniz.");
+                return;
+            }
+            Bakiye += tutar;
+            Console.WriteLine($"Hesap No {HesapNo} \nBakiye{Bakiye}");
+        }
+        public virtual void ParaCek(decimal tutar)
+        {
+            if(tutar <= 0)
+            {
+                Console.WriteLine("Lütfen geçerli bir tutar giriniz.");
+                return;
+            }
+            else if (tutar > Bakiye)
+            {
+                Console.WriteLine("Hesapta çekilecek tutar bulunmamaktadır");
+            }
+            else
+            {
+                Bakiye -= tutar;
+                Console.WriteLine($"Hesap No {HesapNo} \nBakiye{Bakiye}");
+            }
+        }
+    }
+    class VadeliHesap : BankaHesabi
+    {
+        public override void ParaCek(decimal tutar)
+        {
+            decimal kesintiliTutar = tutar * 1.02m;
+
+            if(kesintiliTutar <= 0)
+            {
+                Console.WriteLine("Geçersiz tutar");
+                return;
+            }
+            else if(kesintiliTutar > Bakiye)
+            {
+                Console.WriteLine("Yetersiz bakiye");
+                return;
+            }
+            else
+            {
+                Bakiye -= kesintiliTutar;
+            }
+            Console.WriteLine($"Kesintili tutar: {kesintiliTutar} - Yeni bakiye: {Bakiye}");
+        }
+    }
+    class VadesizHesap : BankaHesabi
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hesap türü seçiniz: \n1 - Vadeli Hesap\n2 - Vadesiz Hesap");
+            int hesapTur = Convert.ToInt32(Console.ReadLine());
+            
+            BankaHesabi hesap1 = new BankaHesabi();
+            
+
+
+            if (hesapTur == 1)
+            {
+                hesap1 = new VadeliHesap();
+            }
+            else if (hesapTur == 2)
+            {
+                hesap1 = new VadesizHesap();
+            }
+            else
+            {
+                Console.WriteLine("Girilen değer yanlış lütfen yeni değer giriniz");
+                return;
+            }
+            Console.Write("Hesap numarasını giriniz: ");
+            hesap1.HesapNo = Console.ReadLine();
+            hesap1.Bakiye = 5000;
+
+            Console.WriteLine("Hesap oluşturuldu. Hesap No: " + hesap1.HesapNo);
+            Console.WriteLine("Yapmak istediğiniz işlemi seçiniz: \n1. Para Yatır\n2. Para Çek");
+            int secim = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Hesap No: " + hesap1.HesapNo);
+            Console.WriteLine("Mevcut Bakiye: " + hesap1.Bakiye);
+            decimal tutar;
+            switch (secim)
+            {
+                case 1:
+                    Console.Write("Yatırmak istediğiniz tutarı giriniz: ");
+                    tutar = Convert.ToDecimal(Console.ReadLine());
+                    hesap1.ParaYatir(tutar);
+                    break;
+                case 2:
+                    Console.Write("Çekmek istediğiniz tutarı giriniz: ");
+                    tutar = Convert.ToDecimal(Console.ReadLine());
+                    hesap1.ParaCek(tutar);
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("İşlem tamamlandı. Güncel Bakiye: " + hesap1.Bakiye);
+            Console.WriteLine("Çıkmak için herhangi bir tuşa basınız...");
+            Console.ReadKey();
+        }   
+    }
+}
--- 14-Inheritance/Program.cs
+++ 14-Inheritance/Program.cs
@@ -6,10 +6,97 @@ using System.Threading.Tasks;
 
 namespace _14_Inheritance
 {
+    //Kalıtım - Inheritance 
+    class AtaSinif
+    {
+        public AtaSinif()
+        {
+            Console.WriteLine("AtaSinif kurucu metodu çalıştı.");
+        }
+        public void Yaz()
+        {
+            Console.WriteLine("AtaSinif Yaz metodu çalıştı.");
+        }
+    }
+    //AtaSinif'tan OgulSinif'a kalıtım yapıldı. OgulSinif, AtaSinif'ın tüm özelliklerini ve metotlarını miras alır.
+    class OgulSinif : AtaSinif
+    {
+        public OgulSinif()
+        {
+            Console.WriteLine("OgulSinif kurucu metodu çalıştı.");
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            OgulSinif ogul = new OgulSinif();
+            ogul.Yaz(); // OgulSinif, AtaSinif'tan miras aldığı Yaz() metodunu kullanabilir.
+
+            ApartmanKati apartmanKati = new ApartmanKati();
+            apartmanKati.sahip = "Mehmet Bey";
+            apartmanKati.EvSahibiniYaz(apartmanKati.sahip);
+
+            apartmanKati.apartmanYoneticisi = "Ahmet Bey";
+            apartmanKati.ApartmanYoneticisiniYaz(apartmanKati.apartmanYoneticisi);
+
+            Cizici[] birCizici = new Cizici[4];
+            birCizici[0] = new DogruCiz();
+            birCizici[1] = new DaireCiz();
+            birCizici[2] = new KareCiz();
+            birCizici[3] = new Cizici();
+
+            foreach (var item in birCizici)
+            {
+                item.Ciz();
+            }
+
+            Console.Read();
+        }
+    }
+    class Ev
+    {
+        public string sahip;
+        public void EvSahibiniYaz(string sahip)
+        {
+            Console.WriteLine("Ev sahibi: " + sahip);
+        }
+    }
+    class ApartmanKati : Ev
+    {
+        internal string apartmanYoneticisi;
+        public void ApartmanYoneticisiniYaz(string yonetici)
+        {
+            Console.WriteLine("Apartman Yöneticisi: " + yonetici);
+        }
+    }
+    //Polimorfizm (Çok Biçimlilik) - Polymorphism
+    public class Cizici
+    {
+        public virtual void Ciz() // virtual anahtar kelimesi, bu metotun alt sınıflarda geçersiz kılınabileceğini belirtir.
+        {
+            Console.WriteLine("Çizici");
+        }
+    }
+    public class DogruCiz : Cizici
+    {
+        public override void Ciz() // override anahtar kelimesi ile ciz metodumuzu ezerek farklı çıktı üretmesini sağladık
+        {
+            Console.WriteLine("Düz çizgi");
+        }
+    }
+    public class DaireCiz : Cizici
+    {
+        public override void Ciz() // override anahtar kelimesi ile ciz metodumuzu ezerek farklı çıktı üretmesini sağladık
+        {
+            Console.WriteLine("Daire");
+        }
+    }
+    public class KareCiz : Cizici
+    {
+        public override void Ciz() // override anahtar kelimesi ile ciz metodumuzu ezerek farklı çıktı üretmesini sağladık
+        {
+            Console.WriteLine("Kare");
         }
     }
 }
