3610eb7d53ece2af0c3df9db77fce3c2715ba8ba
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-23 16:57:49 +0300

    Started Interfaces topic and created a basic example demonstrating interface implementation with a 2D point coordinate system

---
+++ 16-Interfaces/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    interface OrnekArayuz
+    {
+        //interface içerisinde property de public otomatik gelir 
+        int MyProperty { get; set; }
+    }
+    interface Idemo
+    {
+        void Goster();//Interface lerde sadece metot imzaları yer alır, metotlar bu interface den miras alan class larda kullanılır.
+    }
+    interface Inokta
+    {
+        //Özgenlerin imzaları
+        int x { get; set; }
+        int y { get; set; }
+    }
+    //Inokta arayüzünü implement eden sınıfımız
+    class Nokta : Inokta
+    {
+        //Alanlar fields
+        private int _x {  get; set; }
+        private int _y { get; set; }
+
+        public int x 
+        { 
+            get { return _x; } 
+            set { _x = value; } 
+        }
+        public int y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 16-PekistirmeOrnekler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_PekistirmeOrnekler
+{
+    //2D Nokta Koordinat Sistemi
+    /*  
+        🎯 Senaryomuz
+        Harita / oyun / çizim uygulaması geliştiriyorsun
+        Her noktanın x ve y koordinatı olmak zorunda
+    */
+    interface Inokta
+    {
+        int X { get; set; }
+        int Y { get; set; }
+    }
+    class Nokta2D : Inokta
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Inokta n = new Nokta2D();
+            n.X = 5;
+            n.Y = 10;
+            Console.WriteLine(n.X + n.Y);
+
+            Console.Read();
+        }
+    }
+}
