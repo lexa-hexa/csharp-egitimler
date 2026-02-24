1de7eaf3662919b387c6efdafee3579cbf1345ee
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-24 15:39:10 +0300

    Abstract classes completed. Implemented Employee example demonstrating inheritance, overrides, and polymorphism.

---
+++ 17-AbstractClasses/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Oracle();
+            database.Add();
+            database.Delete();
+
+            Database sql = new SqlServer();
+            sql.Add();
+            sql.Delete();
+
+            Console.Read();
+        }
+    }
+    abstract class Database
+    {
+        public void Add()
+        {
+            Console.WriteLine("Ekleme başarılı!");
+        }
+        public abstract void Delete();
+
+        public abstract void Update();
+
+        public abstract void Get();
+    }
+    class SqlServer : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Kayıt sql dan silindi!");
+        }
+
+        public override void Get()
+        {
+            Console.WriteLine("Kayıt sql serverdan getirildi");
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("Kayıt sql server da güncellendi!");
+        }
+    }
+    class Oracle : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Kayıt oracle dan silindi!");
+        }
+
+        public override void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class MySql : Database
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Kayıt mysql den silindi!");
+        }
+
+        public override void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 17-CalisanSistemiOrnek/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_CalisanSistemiOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee dev = new Developer();
+
+            dev.Name = "Test";
+            dev.Salary = 3000;
+            dev.Info();
+            dev.CalculateBonus();
+        }
+    }
+    abstract class Employee
+    {
+        public string Name {  get; set; }
+        public decimal Salary {  get; set; }
+        public void Info() 
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Salary);
+        }
+        public abstract void CalculateBonus();
+    }
+    class Developer : Employee
+    {
+        public override void CalculateBonus()
+        {
+            decimal bonus = Salary * 0.10m;
+            Console.WriteLine($"Alınan bonus: {bonus}");
+        }
+    }
+    class Manager : Employee
+    {
+        public override void CalculateBonus()
+        {
+            decimal bonus = Salary * 0.30m;
+            Console.WriteLine($"Alınan bonus: {bonus}");
+        }
+    }
+    class Intern : Employee
+    {
+        public override void CalculateBonus()
+        {
+            Console.WriteLine("Intern için bonus yok");
+        }
+    }
+}
