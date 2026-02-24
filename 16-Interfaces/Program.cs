69565519e3444634fbadc34c19d904d6ebfab6de
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-24 14:55:49 +0300

    Interface topic completed. Implemented notification example demonstrating multiple implementations and runtime polymorphism.

--- 16-Interfaces/Program.cs
+++ 16-Interfaces/Program.cs
@@ -39,11 +39,111 @@ namespace _16_Interfaces
             set { _y = value; }
         }
     }
+    interface icerebilecekleri : Idemo
+    {
+        //Bir interface in içerebileceği özellikler 
+        //public int MyProperty { get; set; } bir interface içerisine prop tab tab ile property eklersek başındaki public ifadesini kaldırmak zorundayız çünkü interfaceler erişim belirteci almaz daima public tir zaten
+        int Sayi { get; set; } //Interface ler erişim belirteci olmadan propertyleri barındırabilir
+        // static int Sayi2 {get; set;}
+        void MesajVer(); //Interface ler metot imzalarını barındırabilir
+        //string metin; interface ler içerisinde değişken kullanılamaz
+        //Interface ler class lardaki kurucu metotları da kullanamaz
+        int CikarmaYap(int sayi1, int sayi2); //Interface ler parametreli metot imzası alabilir
+        /*int CikarmaYap(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2; //Interface ler içerisinde metotların yapacağı işler tanımlanmaz
+        }*/
+    }
+    class ArayuzKullanimi : icerebilecekleri
+    {
+        public int Sayi { get; set; }
+
+        public int CikarmaYap(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+
+        public void Goster()
+        {
+            Console.WriteLine("interface de imzası tanımlanan metodun yapacağı iş class da belirlenir");
+        }
+
+        public void MesajVer()
+        {
+            throw new NotImplementedException();
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
 
         }
+    }
+    interface ISinifGereksinimleri
+    {
+        int Id { get; set; }
+        int Name { get; set; }
+        int Description { get; set; }
+        DateTime CreateAt { get; set; }
+        DateTime UpdateAt { get; set; }
+    }
+    interface IVeritabaniIslemleri
+    {
+        void Add();
+        void Update();
+        void Delete();
+        void Get();
+        void GetAll();
+    }
+    class Kategori : ISinifGereksinimleri, IVeritabaniIslemleri
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Urun : ISinifGereksinimleri
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public decimal Price { get; set; }
+    }
+    class Marka : ISinifGereksinimleri
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Description { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
--- 16-PekistirmeOrnekler/Program.cs
+++ 16-PekistirmeOrnekler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,12 +27,75 @@ namespace _16_PekistirmeOrnekler
     {
         static void Main(string[] args)
         {
+            /*
             Inokta n = new Nokta2D();
             n.X = 5;
             n.Y = 10;
             Console.WriteLine(n.X + n.Y);
+            */
+            INotification notification = null;
 
-            Console.Read();
+            Console.Write("Mesaj: ");
+            string message = Console.ReadLine();
+
+            int yontem;
+
+            do
+            {
+                Console.WriteLine("Lütfen bildirim gönderim tipi seçiniz: \n0-Çıkış\n1-Email Gönderim\n2-Sms Gönderim\n3-Push Gönderim");
+                yontem = Convert.ToInt32(Console.ReadLine());
+
+                if (yontem < 0 || yontem > 3)
+                {
+                    Console.WriteLine("Hatalı değer girdiniz");
+                }
+                if (yontem == 1)
+                {
+                    notification = new EmailNotification();
+                    notification.Send(message);
+                    break;
+                }
+                if (yontem == 2)
+                {
+                    notification = new SmsNotification();
+                    notification.Send(message);
+                    break;
+                }
+                if (yontem == 3)
+                {
+                    notification = new PushNotification();
+                    notification.Send(message);
+                    break;
+                }
+            } while (yontem != 0);
+            
+
+                Console.Read();
+        }
+    }
+    interface INotification
+    {
+        void Send(string message);
+    }
+    class EmailNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Email gönderildi!");
+        }
+    }
+    class SmsNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Sms gönderildi!");
+        }
+    }
+    class PushNotification : INotification
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine("Push gönderildi!");
         }
     }
 }
