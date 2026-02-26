03745d157d9f2962748b75ef6413ee909b31e565
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-26 21:14:02 +0300

    Collections practice completed and Employee management console app implemented with List and Dictionary (CRUD operations)

---
+++ 18-Collections/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ornek1();
+            Ornek2();
+            Ornek3();
+            Ornek4();
+            Ornek5();
+            Ornek6();
+            Ornek7();
+            Ornek8();
+            Ornek9();
+            ListKullanimi();
+
+            Console.Read();
+        }
+        static void Ornek1()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(12);
+            arrayList.Add(14);
+            arrayList.Add(16);
+
+            foreach (var item in arrayList) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(arrayList[1]);
+        }
+        static void Ornek2()
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("İzmir");
+            arrayList.Add("Ankara");
+            arrayList.Add("İstanbul");
+            Console.WriteLine("Sıralanmamış Liste");
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Sıralanmış Liste");
+            arrayList.Sort();//A dan Z ye sıralama için kullanıyoruz
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Ters Sıralanmış Liste");
+            arrayList.Reverse();//Z den A ya sıralama için kullanıyoruz
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine();
+            }
+        }
+        static void Ornek3()
+        {
+            //Koleksiyonlara farklı veri tiplerinden değerler atama
+            int tamSayi = 20;
+            double dabil = 10.8;
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add("Başkent Ankara");
+            arrayList.Add(tamSayi);
+            arrayList.Add(dabil);
+
+            for (int i = 0; i < arrayList.Count; i++) 
+            {
+                Console.WriteLine(arrayList[i]);
+            }
+
+
+        }
+        static void Ornek4()
+        {
+            StringCollection sc = new StringCollection();
+            sc.Add("Ankara");
+            sc.Add("İstanbul");
+            sc.Add("İzmir");
+            foreach (var item in sc)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Ornek5()
+        {
+            StringDictionary stringDictionary = new StringDictionary();
+            stringDictionary.Add("06", "Ankara");
+            stringDictionary.Add("34", "İstanbul");
+            stringDictionary.Add("35", "İzmir");
+            Console.WriteLine(stringDictionary["06"]);
+            foreach (var item in stringDictionary.Values)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in stringDictionary.Keys)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Ornek6()
+        {
+            Stack stack = new Stack();//Stack sınıfı programlamada LIFO (last in first out) son giren ilk çıkar diye bilinen önemli bir yapıdır.
+            stack.Push("Ankara");
+            stack.Push("İstanbul");
+            stack.Push("İzmir");
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Pop());//Stack in üstünden bir öğe çekmek için pop metodu kullanılır
+            }
+        }
+        static void Ornek7() 
+        {
+            //Queue sınıfı programlamada FIFO (First in first out, ilk giren ilk çıkar) diye bilinen bir yapısıdır.
+            Queue queue = new Queue();
+            queue.Enqueue("Ankara");
+            queue.Enqueue("İstanbul");
+            queue.Enqueue("İzmir");
+
+            while(queue.Count > 0)
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+        static void Ornek8()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(06, "Ankara");
+            hashtable.Add(34, "İstanbul");
+            hashtable.Add(35, "İzmir");
+            Console.WriteLine("Anahtarlar");
+            foreach (var item in hashtable.Keys)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Değerler");
+            foreach (var item in hashtable.Values)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Arama yapmak için değeri girin: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(hashtable[n]);
+        }
+        static void Ornek9()
+        {
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            keyValuePairs.Add("book","Kitap");
+            keyValuePairs.Add("table", "masa");
+            keyValuePairs.Add("computer", "bilgisayar");
+            keyValuePairs.Add("laptop", "dizüstü");
+            keyValuePairs.Add("tv", "televizyon");
+
+            Console.WriteLine(keyValuePairs["table"]);
+
+
+            Dictionary<int, string> keyValues = new Dictionary<int, string>();
+            keyValues.Add(01, "Adana");
+            keyValues.Add(06, "Ankara");
+            keyValues.Add(34, "İstanbul");
+            keyValues.Add(35, "İzmir");
+            Console.WriteLine(keyValues[35]);
+
+            foreach (var item in keyValuePairs) 
+            {
+                Console.WriteLine($"Anahtar: {item.Key} | Değer: {item.Value}");
+            }
+            Console.WriteLine(keyValuePairs.ContainsKey("gozluk"));
+            Console.WriteLine(keyValuePairs.ContainsKey("table"));
+        }
+        class User
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+        static void ListKullanimi()
+        { 
+            List<string> sehirler = new List<string>();
+            sehirler.Add("İstanbul");
+            sehirler.Add("Ankara");
+            sehirler.Add("İzmir");
+            Console.WriteLine(sehirler.Contains("Adana"));
+            foreach (var item in sehirler) 
+            { 
+                Console.WriteLine(item); 
+            }
+            List<User> users = new List<User>();
+            users.Add(new User { Id = 1, FirstName = "Enes", LastName = "Yerebakan"});
+            users.Add(new User { Id = 2, FirstName = "Polat", LastName = "Alemdar" });
+            users.Add(new User { Id = 3, FirstName = "Memati", LastName = "Baş" });
+
+            List<User> kullanicilar = new List<User>()
+            {
+                new User { Id = 1, FirstName = "Enes", LastName = "Yerebakan" },
+                new User { Id = 2, FirstName = "Polat", LastName = "Alemdar" },
+                new User { Id = 3, FirstName = "Memati", LastName = "Baş" }
+            };
+            var yeniKullanici = new User
+            {
+                Id = 4, FirstName = "Abdulhey", LastName = "Gezer"
+            };
+            Console.WriteLine(kullanicilar.Contains(yeniKullanici));
+            kullanicilar.Add(yeniKullanici);
+            kullanicilar.AddRange(new User[2]
+            {
+                new User { Id = 1, FirstName = "Enes", LastName = "Yerebakan" },
+                new User { Id = 2, FirstName = "Polat", LastName = "Alemdar" },
+            });
+            kullanicilar.AddRange(users);
+            Console.WriteLine(kullanicilar.Contains(yeniKullanici));
+
+            var index = kullanicilar.IndexOf(yeniKullanici);
+            Console.WriteLine("yeniKullanici index numarası: " + index);
+
+            kullanicilar.Add(yeniKullanici);
+            var sonindex = kullanicilar.LastIndexOf(yeniKullanici);
+            Console.WriteLine("yeniKullanici son index numarası: " + sonindex);
+
+            kullanicilar.Insert(1, yeniKullanici);
+            kullanicilar.Remove(yeniKullanici);//Listeden eleman silmek için kullanılır
+            //Insert ve Add farkı
+            //Add listenin sonuna kullanıcı eklemesi yapıyor 
+            //Insert biz başta nereye atamak istediğimizi belirtiyoruz
+
+            foreach (var item in kullanicilar)
+            {
+                Console.WriteLine("Id: "+ item.Id + " Adı: " + item.FirstName);
+            }
+
+            var count = kullanicilar.Count;//kullanicilar listesindeki eleman sayısını göstermek için count kullanılır
+            Console.WriteLine($"kullanicilar listesindeki eleman sayısı {count} adettir.");
+        }
+    }
+}
---
+++ 18-PekistirmeOrnekler/Program.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_PekistirmeOrnekler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ogrenciler();
+            Console.WriteLine("Merhabalar Enes'in Harikalar diyarına hoş geldin");
+            Console.WriteLine("Lütfen yapmak istediğin işlemi seç:");
+            Console.WriteLine("\n");
+            int islem;
+            do
+            {
+                Console.WriteLine("1 - Çalışan Ekle\n2 - Id ile çalışan bul\n3 - Id ile çalışan sil\n4 - Çalışanları Listele\n5 - Çalışan bilgisi güncelleme\n0 - Çıkış");
+                islem = Convert.ToInt32(Console.ReadLine());
+                switch (islem)
+                {
+                    case 1:
+                        Personels();
+                        break;
+                    case 2:
+                        FindEmployee();
+                        break;
+                    case 3:
+                        DeleteEmployee();
+                        break;
+                    case 4:
+                        ListEmployee();
+                        break;
+                    case 5:
+                        UpdateEmployee();
+                        break;
+                    case 0:
+                        Console.WriteLine("Çıkış yapılıyor");
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçim, Lütfen doğru seçim yaptığınızdan emin olun!");
+                        break;
+                }
+            } while (islem != 0);
+
+            Console.Read();
+        }
+        class Students
+        {
+            public int Id { get; set; }
+            public string Adi { get; set; }
+            public string Soyadi { get; set; }
+            public string Bolum { get; set; }
+        }
+        static void Ogrenciler()
+        {
+            List<Students> students = new List<Students>();
+            students.Add(new Students { Id = 1, Adi = "Enes", Soyadi = "Yerebakan", Bolum = "Yazılım Geliştirme" });
+            students.Add(new Students { Id = 2, Adi = "Polat", Soyadi = "Alemdar", Bolum = "Proje Geliştirme" });
+            students.Add(new Students { Id = 3, Adi = "Memati", Soyadi = "Baş", Bolum = "Ürün Geliştirme" });
+
+            Console.WriteLine("Aradığınız öğrencinin id sini giriniz:");
+            int id = int.Parse(Console.ReadLine());
+            bool bulunduMu = false;
+
+
+            foreach (Students student in students)
+            {
+                if (student.Id == id)
+                {
+                    bulunduMu = true;
+                    Console.WriteLine($"Id: {student.Id} | Adı: {student.Adi} | Soyadı: {student.Soyadi} | Bölümü: {student.Bolum}");
+                    break;
+                }
+            }
+            if (!bulunduMu)
+            {
+                Console.WriteLine("Kullanıcı bulunmamaktadır");
+            }
+            /*
+            👉 List = sıra
+            👉 Arama = dolaşma
+            👉 Flag = sonuç bilgisi
+            👉 break = erken durdurma
+            👉 Döngü sonrası = karar
+            👉 Object = veri kabı
+
+            Bu 6 madde collections’ın temelidir.
+            */
+
+            //Soru: Eğer listede 1000 öğrenci varsa, Id ile arama yaparken neden listeyi dolaşmak zorundayız?
+            //Cevap: 
+            /*
+                 Listede Id ile arama yaparken listeyi dolaşmak zorundayız çünkü:
+
+                👉 List elemanların Id’ye göre nerede olduğunu bilmez
+                👉 Id’ye göre indeksleme yapmaz
+                👉 Sadece sırayla tutar
+
+                Yani programın elinde şu bilgi yok:
+
+                Id 74 → index 73
+
+                Bu yüzden tek yöntem:
+
+                👉 Baştan sona bakmak
+
+                Bu algoritmaya:
+
+                Linear search (doğrusal arama) denir.
+             */
+
+            //Soru: Eğer Id ile hızlı arama yapmak istiyorsak neden List yerine Dictionary kullanırız?
+            //Cevap: Dictionary’de her veri bir key ile eşleştiği için arama yapılırken listeyi dolaşmaya gerek kalmaz, key üzerinden doğrudan ilgili değere ulaşılır ve bu nedenle List’e göre daha hızlıdır.
+
+            //👉 List = aramak için dolaşılır
+            //👉 Dictionary = anahtar ile direkt bulunur
+
+            // ================= LIST vs DICTIONARY SEBEPLER =================
+
+            // 1️ - Sıralı veri ihtiyacı
+            // List verileri eklenme sırasına göre tutar.
+            // UI’de listeleme, blog yazıları, sipariş geçmişi gibi durumlarda sıralama önemlidir.
+            // Dictionary sıralı değildir, bu yüzden sıralı gösterim gereken yerde List kullanılır.
+
+            // 2️ - Key zorunluluğu
+            // Dictionary kullanmak için her veri için benzersiz bir key gerekir.
+            // Ancak bazı senaryolarda key yoktur veya önemli değildir.
+            // Bu durumlarda List daha uygun bir veri yapısıdır.
+
+            // 3️ - Memory kullanımı
+            // Dictionary hash table yapısı kullandığı için List’e göre daha fazla memory tüketir.
+            // Basit veri saklama işlemlerinde List daha hafiftir.
+
+            // 4️ - Duplicate veri ihtiyacı
+            // Dictionary’de key benzersiz olmak zorundadır.
+            // Aynı key tekrar eklenemez.
+            // Ancak List duplicate veri tutabilir (aynı öğrenci, aynı ürün vb).
+
+            // 5️ - Basitlik ve kullanım kolaylığı
+            // List daha basit bir veri yapısıdır.
+            // Iterasyon (foreach), veri ekleme ve saklama işlemleri kolaydır.
+            // Bu yüzden genel veri saklama senaryolarında List tercih edilir.
+
+            // ================= MENTAL MODEL =================
+
+            // List = sıralı veri koleksiyonu
+            // Dictionary = key ile hızlı erişim (lookup)
+
+
+            //Soru: Öğrenci sisteminde hangi işlem için Dictionary kullanmak daha mantıklı olur ve neden?
+        }
+        class Employee
+        {
+            public int Id { get; set; }
+            public string Adi { get; set; }
+            public string Soyadi { get; set; }
+            public string Departman { get; set; }
+            public decimal Maas { get; set; }
+        }
+        static List<Employee> employees = new List<Employee>();
+        static Dictionary<int, Employee> employeeById = new Dictionary<int, Employee>();
+        static void Personels()
+        {
+            Console.WriteLine("Lütfen Id numarası giriniz");
+            int id = int.Parse(Console.ReadLine());
+
+
+
+            if (employeeById.ContainsKey(id))
+            {
+                Console.WriteLine("Bu kullanıcı kayıtlı!");
+            }
+            else
+            {
+                Employee emp = new Employee();
+                emp.Id = id;
+                Console.WriteLine("Yeni kullanıcı oluşturuluyor lütfen bilgileri eksiksiz giriniz.");
+                Console.Write("Ad: ");
+                emp.Adi = Console.ReadLine();
+                Console.Write("Soyad: ");
+                emp.Soyadi = Console.ReadLine();
+                Console.Write("Departman: ");
+                emp.Departman = Console.ReadLine();
+                Console.Write("Maaş bilgisi: ");
+                emp.Maas = decimal.Parse(Console.ReadLine());
+
+                employees.Add(emp);
+                employeeById.Add(id, emp);
+                Console.WriteLine("Yeni kullanıcı eklendi!");
+            }
+
+        }
+        static void FindEmployee()
+        {
+            Console.Write("Merhaba, lütfen aradığınız çalışan id sini girer misiniz: ");
+            int id = int.Parse(Console.ReadLine());
+
+            if (employeeById.ContainsKey(id))
+            {
+                Console.WriteLine("Personelin dictionary ile aranması sonucu");
+                Employee emp = employeeById[id];
+                Console.WriteLine($"Id: {emp.Id} | Ad: {emp.Adi} | Soyad: {emp.Soyadi} | Departman: {emp.Departman} | Maaş: {emp.Maas}");
+            }
+            else { Console.WriteLine("Böyle bir kullanıcı bulunmamaktadır"); }
+
+            Console.WriteLine("Liste ile arama");
+            bool bulunduMu = false;
+            foreach (Employee n in employees)
+            {
+                if (n.Id == id)
+                {
+                    bulunduMu = true;
+                    Console.WriteLine($"Id: {n.Id} | Ad: {n.Adi} | Soyad: {n.Soyadi} | Departman: {n.Departman} | Maaş: {n.Maas}");
+                    break;
+                }
+            }
+            if (!bulunduMu)
+            {
+                Console.WriteLine("Liste'de kişi bulunamadı");
+            }
+
+
+        }
+        static void DeleteEmployee()
+        {
+            Console.Write("Merhabalar, lütfen silmek istediğiniz kişinin id numarasını giriniz:");
+            int id = int.Parse(Console.ReadLine());
+
+            if (employeeById.ContainsKey(id))
+            {
+                Employee emp = employeeById[id];
+                Console.WriteLine("Personelin dictionaryden silinmesi");
+                employeeById.Remove(id);
+                employees.Remove(emp);
+                Console.WriteLine($"Silindi: {emp.Id} {emp.Adi} {emp.Soyadi} {emp.Departman} {emp.Maas}");
+            }
+
+            //Dictionary: Id → objeyi bulur
+            //List: objeyi veya indexi kullanarak siler
+            //Biz: Id ile objeyi dictionary’den bulup, objeyi listeden siliyoruz
+
+            else { Console.WriteLine("Böyle bir kullanıcı maalesef veri tabanımızda bulunmuyor"); }
+        }
+        static void ListEmployee()
+        {
+            if(employees.Count == 0)
+            {
+                Console.WriteLine("Kayıt yok");
+            }
+            else
+            {
+                foreach (var user in employees)
+                {
+                    Console.WriteLine($"Id: {user.Id} | Adı: {user.Adi} | Soyadı: {user.Soyadi} | Departman: {user.Departman} | Maaş: {user.Maas}");
+                }
+            }
+        }
+        static void UpdateEmployee()
+        {
+            Console.WriteLine("Lütfen bilgilerini güncellemek istediğiniz kişinin id numarasını giriniz.");
+            int id = int.Parse(Console.ReadLine());
+
+            if (employeeById.ContainsKey(id))
+            {
+                Employee emp = employeeById[id];
+                bool devam = true;
+                do
+                {
+                    Console.WriteLine("Kişinin hangi bilgisini güncellemek istiyorsunuz?");
+                    Console.WriteLine("1 - Ad\n2 - Soyad\n3 - Departman\n4 - Maaş\n0 - Çıkış");
+                    int islem = int.Parse(Console.ReadLine());
+                    switch (islem)
+                    {
+                        case 0:
+                            Console.WriteLine("Çıkılıyor..");
+                            devam = false;
+                            break;
+                        case 1:
+                            Console.Write("Yeni ad:");
+                            string ad = Console.ReadLine();
+                            emp.Adi = ad;
+                            Console.WriteLine("Güncelleme yapıldı!");
+                            break;
+                        case 2:
+                            Console.Write("Yeni Soyad:");
+                            string soyad = Console.ReadLine();
+                            emp.Soyadi = soyad;
+                            Console.WriteLine("Güncelleme yapıldı!");
+                            break;
+                        case 3:
+                            Console.Write("Yeni Departman:");
+                            string departman = Console.ReadLine();
+                            emp.Departman = departman;
+                            Console.WriteLine("Güncelleme yapıldı!");
+                            break;
+                        case 4:
+                            Console.Write("Yeni Maaş:");
+                            decimal maas = decimal.Parse(Console.ReadLine());
+                            emp.Maas = maas;
+                            Console.WriteLine("Güncelleme yapıldı!");
+                            break;
+                        default:
+                            Console.WriteLine("Hata oluştu!");
+                            break;
+                    }
+                    
+
+
+                } while (devam);
+                
+            }
+            else { Console.WriteLine("Veritabanında böyle biri yok"); }
+
+        }
+    }
+}
