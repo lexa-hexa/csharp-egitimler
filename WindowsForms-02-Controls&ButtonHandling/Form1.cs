8624c116e8715e7c2370c4dc9b8b019391767655
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-28 00:49:50 +0300

    WinForms basics: form navigation, timer color change, and keyboard shortcuts

---
+++ WindowsForms-02-Controls&ButtonHandling/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_02_Controls_ButtonHandling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            //form2.Show();//Form2 penceresini göster 
+            form2.ShowDialog();//Show dan farkı form2 yi açtığımızda bir önceki forma ikinci form ekranı kapanmadan açamıyorsun
+            //this.Hide();formu kapatsanda arka planda çalışmaya devam eder
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Form ekranında mouse ile çift tıklayarak bu metodu oluşturduk
+            this.Close(); //ilgili pencereyi kapat
+            //Form1.ActiveForm.Close();
+        }
+    }
+}
---
+++ WindowsForms-02-Controls&ButtonHandling/Form3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_02_Controls_ButtonHandling
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();// Random rastgele değer üretmemizi sağlar
+            this.BackColor = Color.FromArgb(random.Next(1, 100), random.Next(1, 100), random.Next(1, 100));
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = 500;
+        }
+
+        private void Form3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F4)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form3_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                MessageBox.Show("Form Kapatılacak!", "Uyarı!");
+                this.Close();
+            }
+        }
+    }
+}
