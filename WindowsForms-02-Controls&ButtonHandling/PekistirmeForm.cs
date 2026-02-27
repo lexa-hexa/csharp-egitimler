4d1f6743fa7b86e7cd6a4094749e3c154fb8201f
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-02-28 01:22:39 +0300

    WinForms event handling and MessageBox decision flow practice completed

---
+++ WindowsForms-02-Controls&ButtonHandling/PekistirmeForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_02_Controls_ButtonHandling
+{
+    public partial class PekistirmeForm : Form
+    {
+        public PekistirmeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PekistirmeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc;
+            sonuc = MessageBox.Show("Çıkmak istiyor musun?", "Uyarı!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sonuc == DialogResult.Yes) 
+            {
+                this.Close();
+            }
+            else { MessageBox.Show("İptal edildi!"); }
+        }
+    }
+}
