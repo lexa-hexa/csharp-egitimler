306db6ebaaa170d68670a9b2c6f37520e42f5b3c
Enes <114726412+lexa-hexa@users.noreply.github.com>
2026-03-01 23:00:47 +0300

    Login screen with validation and 3-attempt limit logic implemented

---
+++ WindowsForms-03-KullaniciGirisEkrani/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsForms_03_KullaniciGirisEkrani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.KalanHak = new System.Windows.Forms.Label();
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtKullaniciKodu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.KalanHak);
+            this.groupBox1.Controls.Add(this.btnGiris);
+            this.groupBox1.Controls.Add(this.txtKullaniciKodu);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSifre);
+            this.groupBox1.Controls.Add(this.txtKullaniciAdi);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(215, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(307, 190);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kullanıcı Giriş";
+            // 
+            // KalanHak
+            // 
+            this.KalanHak.AutoSize = true;
+            this.KalanHak.Location = new System.Drawing.Point(23, 149);
+            this.KalanHak.Name = "KalanHak";
+            this.KalanHak.Size = new System.Drawing.Size(69, 13);
+            this.KalanHak.TabIndex = 7;
+            this.KalanHak.Text = "Kalan Hak: 3";
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(103, 144);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 23);
+            this.btnGiris.TabIndex = 6;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
+            // 
+            // txtKullaniciKodu
+            // 
+            this.txtKullaniciKodu.Location = new System.Drawing.Point(103, 96);
+            this.txtKullaniciKodu.Name = "txtKullaniciKodu";
+            this.txtKullaniciKodu.Size = new System.Drawing.Size(100, 20);
+            this.txtKullaniciKodu.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kullanıcı Kodu";
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(103, 63);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(100, 20);
+            this.txtSifre.TabIndex = 4;
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(103, 32);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(100, 20);
+            this.txtKullaniciAdi.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı adı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Kullanıcı Giriş Ekranı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtKullaniciKodu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label KalanHak;
+    }
+}
+
---
+++ WindowsForms-03-KullaniciGirisEkrani/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_03_KullaniciGirisEkrani
+{
+    public partial class Form1 : Form
+    {
+        int hak = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiris_Click(object sender, EventArgs e)
+        {
+            if (hak <= 0) return;
+            if (!AlanlarDoluMu()) return;
+            if (!GirisBasariliMi()) return;
+        }
+        private bool AlanlarDoluMu()
+        {
+            if (string.IsNullOrWhiteSpace(txtKullaniciAdi.Text))//Kullanıcı adı boşsa
+            {
+                MessageBox.Show("Kullanıcı adı boş geçilemez!");
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(txtSifre.Text))
+            {
+                MessageBox.Show("Şifre boş geçilemez");
+                return false;
+            }
+            else if (string.IsNullOrWhiteSpace(txtKullaniciKodu.Text))
+            {
+                MessageBox.Show("Kullanıcı kodu boş geçilemez");
+                return false;
+            }
+            else { return true; }
+        }
+        private bool GirisBasariliMi()
+        {
+            string veritabanindakiKullaniciAdi = "Enes";
+            string veritabanindakiSifre = "123456";
+            string veritabanindakiKullaniciKodu = "Axd1234";
+
+            if (txtKullaniciAdi.Text == veritabanindakiKullaniciAdi && txtKullaniciKodu.Text == veritabanindakiKullaniciKodu && txtSifre.Text == veritabanindakiSifre)
+            {                
+                MessageBox.Show("Hoşgeldin " + txtKullaniciAdi.Text);
+                groupBox1.Visible = false;
+                return true;
+            }
+            else
+            {
+                hak--;
+                HakGuncelle();
+                if (hak > 0)
+                {
+                    
+                    MessageBox.Show($"Hatalı giriş. Kalan hak: {hak}", "Uyarı!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return false;
+                }
+                else
+                {                    
+                    btnGiris.Enabled = false;
+                    txtKullaniciAdi.Enabled = false;
+                    txtSifre.Enabled = false;
+                    txtKullaniciKodu.Enabled = false;
+                    MessageBox.Show("3 Kez hatalı giriş yapıldı! Hesabınız kilitlenmiştir.");
+                    return false;
+                }
+            }
+        }
+        private void HakGuncelle()
+        {
+            KalanHak.Text = "Kalan Hak: " + hak;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HakGuncelle();
+        }
+    }
+}
